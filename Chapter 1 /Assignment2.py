86dcbc8d3fb32ed384be25214857bd8689c1ade1
Shreya Mishra <shreyagmishra007@gmail.com>
2025-12-15 13:34:55 +0530

    Initial course files from fork

--- Chapter 1 /Assignment2.py
+++
@@ -1,8 +0,0 @@
-# Question - Take diameter as input and calculate the area of a circle.
-
-
-diameter = int(input("Inter the value of diameter: "))
-radius = diameter/2
-area = 3.14 * (radius ** 2)
-print("Radius of the circle is", radius)
-print("Area of circle is", area)
--- Chapter 1 /userInput.py
+++
@@ -1,8 +0,0 @@
-# Program to take input from the user 
-
-print("A sample program to understand input method in python")
-
-name= input("Enter your good name :")
-
-print("Your good name is :", name)
-
